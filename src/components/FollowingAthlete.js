39a666cbd0ee475e4a9a60850812f2af6cc89a5f
Mauricio <mcimauricio.coelho@gmail.com>
2020-01-26 19:57:47 +0100

    💅Add style for Following athlete

--- src/components/FollowingAthlete.js
+++ src/components/FollowingAthlete.js
@@ -1,34 +1,39 @@
 import React from "react";
 import useFetch from "fetch-suspense";
-import { Card, Avatar } from "antd";
+import { Card, Avatar, Collapse, Divider, Tag, Descriptions } from "antd";
+
+const { Panel } = Collapse;
 
 function FollowingAthlete({ id }) {
-  console.log(id);
   const athlete = useFetch(
     `https://cf-comp-proxy.herokuapp.com/https://games.crossfit.com/competitions/api/v1/athlete/${id}`
   );
-  console.log(athlete);
+
   return (
     <Card style={{ marginBottom: 4 }}>
       <Card.Meta
         avatar={<Avatar src={athlete.profilePicS3key} />}
         title={athlete.competitorName}
       />
-      {athlete.stats.open.map(open => (
-        <div>
-          <div>{open.year}</div>
-          <div>
-            {open.worldWide.scores.map(score => (
-              <div>
-                <div>wod: {score.ordinal}</div>
-                <div>rank: {score.rank}</div>
-                <div>score: {score.scoreDisplay}</div>
-                <div>scaled: {score.scaled === "0" ? "no" : "yes"}</div>
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
+      <Divider />
+      <Collapse defaultActiveKey={['0']}>
+        {athlete.stats.open.map((open, index) => (
+          <Panel header={open.year} key={index}>
+            <div style={{ display: 'flex', flex: 1, }}>
+              {open.worldWide.scores.map(score => (
+                <div>
+                  <Descriptions title={`WOD ${score.ordinal}`} column={1}>
+                    <Descriptions.Item label="Score">{score.scoreDisplay}</Descriptions.Item>
+                    <Descriptions.Item label="Rank">{score.rank}</Descriptions.Item>
+                  </Descriptions>
+                  {score.scaled === "0" ? <Tag color="blue">RX</Tag> : <Tag color="red">Scale</Tag>}
+                  <Divider />
+                </div>
+              ))}
+            </div>
+          </Panel>
+        ))}
+      </Collapse>
     </Card>
   );
 }
